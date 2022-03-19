7ab29635e1cf6276e199b0fe16ccc7b3faaf420c
Aaron Parecki <aaron@parecki.com>
2022-03-19 17:47:38 +0100

    update code and readme

--- README.md
+++ README.md
@@ -2,26 +2,56 @@
 
 This is a minimal GNAP client in PHP to demonstrate interacting with a GNAP Authorization Server.
 
-This is tested agains the [Java XYZ Server](https://github.com/bspk/oauth.xyz-java)
+This is tested against the [Java GNAP Server](https://github.com/bspk/oauth.xyz-java) hosted at https://gnap-as.herokuapp.com/
+
+
+## Setup
+
+Install PHP dependencies:
+
+```bash
+sudo apt install php8.1 php8.1-mbstring php8.1-curl redis-server
+```
+
+Install the required libraries:
+
+```bash
+composer require phpseclib/phpseclib:~3.0
+composer require web-token/jwt-key-mgmt web-token/jwt-signature
+composer require predis/predis
+```
+
+or just
+
+```bash
+composer install
+```
 
 
 ## How to Run
 
 Generate a private key:
 
-```
-openssl ecparam -genkey -name prime256v1 -noout -out ec256-key.pem
-```
-
-Install the dependencies:
-
-```
-composer install
+```bash
+php gnap-cli.php keygen
 ```
 
-Then run the client from the command line:
+This will generate an RSA private key and save it in a file `private.pem`, and the corresponding public key in `public.pem` and the JWK public key in `public-key.json`.
 
+
+Start a new request:
+
+```bash
+php gnap-cli.php start
 ```
-php gnap-cli.php
+
+If everything works, you should see a URL printed in the console. Open that URL in your browser to authorize the client.
+
+Poll the transaction endpoint until the client gets a token:
+
+```bash
+php gnap-cli.php poll
 ```
+
+After you authorize the client, you should get an access token in the response!
 
--- gnap-cli.php
+++ gnap-cli.php
@@ -1,137 +1,245 @@
 <?php
 require('vendor/autoload.php');
 
-use Jose\Component\Core\JWK;
+use phpseclib3\Crypt\RSA;
+use Bakame\Http\StructuredFields;
 use Jose\Component\KeyManagement\JWKFactory;
-use Jose\Component\Signature\JWSBuilder;
-use Jose\Component\Core\AlgorithmManager;
-use Jose\Component\Signature\Algorithm\ES256;
-use Jose\Component\Signature\Serializer\CompactSerializer;
+use Jose\Component\Core\JWK;
 
-$keyFilename = 'ec256-key.pem';
+define('CLIENT_NAME', 'PHP CLI');
+define('SIG_METHOD', 'pss');
 
-$jwk = JWKFactory::createFromKeyFile($keyFilename);
-$kid = $keyFilename;
+if(!isset($argv[1])) {
+  echo "usage: php sig.php action\n";
+  die(1);
+}
 
+$redis = new \Predis\Client();
+$redis->connect();
 
-$txEndpoint = 'https://xyz-as.herokuapp.com/api/as/transaction';
+$privateKeyFilename = 'private.pem';
+$gnapASEndpoint = 'https://gnap-as.herokuapp.com/api/as/transaction';
+#$gnapASEndpoint = 'http://31.133.128.121:9834/api/as/transaction';
 
+switch($argv[1]) {
 
-$request = [
-  'client' => [
-    'key' => [
-      'proof' => 'jwsd',
-      'jwk' => $jwk->toPublic()->all()
-    ],
-    'display' => [
-      'name' => 'PHP CLI',
-    ],
-  ],
-  'access_token' => [
-    [
-      'label' => 'token1',
-      'access' => ['foo'],
-    ],
-    [
-      'label' => 'token2',
-      'access' => ['bar'],
-    ]
-  ],
-  'interact' => [
-    'start' => ['redirect', 'user_code']
-  ],
-];
+  case 'keygen':
+    $private = RSA::createKey();
+    $public = $private->getPublicKey();
 
-$public = $jwk->toPublic()->all();
-$public['kid'] = $kid;
-$request['client']['key']['jwk']['kid'] = $kid;
+    echo (string)$private."\n";
+    echo (string)$public."\n";
 
-$response = signedRequest($txEndpoint, json_encode($request));
+    file_put_contents($privateKeyFilename, $private);
+    file_put_contents('public.pem', $public);
 
-if(isset($response['interact'])) {
-  echo "Please open this URL in your browser to authorize the application\n";
-  echo $response['interact']['redirect']."\n";
+    $jwk = JWKFactory::createFromKeyFile($privateKeyFilename);
+    $publicJSON = json_encode($jwk->toPublic()->all(), JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES);
+    file_put_contents('public-key.json', $publicJSON);
 
-  print_r($response);
+    break;
 
-  readline("Press enter to continue");
+  case 'start':
+    
+    $client = new GNAPClient($privateKeyFilename, CLIENT_NAME, SIG_METHOD);
+    
+    $response = $client->post($gnapASEndpoint, [
+      'interact' => [
+        'start' => ['redirect', 'user_code']
+      ],
+      'access_token' => [
+        'access' => [
+          [
+            'type' => 'api',
+          ]
+        ]
+      ],
+      // "subject" => [
+      //   "sub_id_formats" => [ "iss_sub", "opaque" ],
+      // ]
+    ]);
+    
+    if(isset($response['interact']['redirect'])) {
+      echo "Visit this URL in your browser:\n";
+      echo $response['interact']['redirect']."\n";
+      
+      $redis->setex('gnap-interaction', 86400, json_encode($response));
+    }
+    
+    break;
 
-  $tokenResponse = null;
-
-  $continue = $response['continue'];
-  while($continue && !isset($response['access_token'])) {
-    echo "Polling: ".$continue['uri']." with AT ".$continue['access_token']['value']."\n";
-    $response = signedRequest($continue['uri'], '', $continue['access_token']['value']);
-    print_r($response);
-
-    #readline("Press enter to continue");
-    sleep(1);
-    $continue = $response['continue'] ?? false;
-  }
-
-  print_r($response);
+  case 'poll':
+    
+    $pending = json_decode($redis->get('gnap-interaction'), true);
+    
+    if(!$pending) {
+      die("No interaction pending\n");
+    }
+    
+    $client = new GNAPClient($privateKeyFilename, CLIENT_NAME, SIG_METHOD);
+    
+    $response = $client->post($pending['continue']['uri'], null, [
+      'Authorization' => 'GNAP '.$pending['continue']['access_token']['value'],
+    ]);
+    
+    $redis->setex('gnap-interaction', 86400, json_encode($response));
+    
+    break;
+  
+  default:
+    die("unknown command\n");
 
 }
 
 
+class GNAPClient {
+  
+  private RSA $rsa;
+  private JWK $jwk;
+  
+  public function __construct(private string $privateKeyFile, private string $name='', private string $method='pss') {
+    if($method == 'pss') {
+      $this->rsa = RSA::loadPrivateKey(file_get_contents($this->privateKeyFile))
+        ->withHash('sha512')
+        ->withMGFHash('sha512')
+        ->withSaltLength(64)
+        ->withPadding(RSA::SIGNATURE_PSS);
+    } else {
+      $this->rsa = RSA::loadPrivateKey(file_get_contents($this->privateKeyFile))
+        ->withHash('sha256')
+        ->withPadding(RSA::SIGNATURE_PKCS1);
+    }
+    $this->jwk = JWKFactory::createFromKeyFile($this->privateKeyFile);
+  }
+  
+  public function setClientName(string $name) {
+    $this->name = $name;
+  }
+  
+  public function get(string $uri, array $headers=[]) {
+    $createdAt = time();
 
-function signedRequest($url, $body, $accessToken=null) {
-  global $jwk, $kid;
 
-  // https://datatracker.ietf.org/doc/html/draft-ietf-gnap-core-protocol#section-7.3.3
-  $jwsHeader = [
-    'kid' => $kid,
-    'alg' => 'ES256',
-    'typ' => 'gnap-binding+jwsd',
-    'htm' => 'POST',
-    'uri' => $url,
-    'created' => time(),
-  ];
-
-  if($accessToken) {
-    $hash = hash('sha256', $accessToken, true);
-    $jwsHeader['ath'] = base64_url_encode(substr($hash, 0, strlen($hash) / 2));
   }
 
-  $algorithmManager = new AlgorithmManager([
-      new ES256(),
-  ]);
+  # https://www.ietf.org/archive/id/draft-ietf-gnap-core-protocol-09.html#name-http-message-signing
+  
+  public function post(string $uri, array|null $params, array $headers=[]) {
+    $createdAt = time();
+    #$createdAt = 1647699014;
 
-  print_r($jwsHeader);
-  echo $body."\n\n";
-  $body_hash = base64_url_encode(hash('sha256', $body, true));
-  echo $body_hash."\n\n";
+    $messageSignatureBase = [
+      '@method' => 'POST',
+      '@target-uri' => $uri,
+    ];
+    
+    if($params) {
+      $params['client'] = $this->_clientProperties();
+      $requestBody = json_encode($params);
+      $bodyDigest = StructuredFields\ByteSequence::fromDecoded(hash('sha256', $requestBody, true));
+      $bodyDigestValue = StructuredFields\Dictionary::fromPairs([['sha-256', $bodyDigest]])->toHttpValue();
 
-  $jwsBuilder = new JWSBuilder($algorithmManager);
-  $jws = $jwsBuilder->create()
-    ->withEncodedPayload($body_hash)
-    ->addSignature($jwk, $jwsHeader)
-    ->build();
+      $messageSignatureBase = array_merge($messageSignatureBase, [
+        'content-digest' => $bodyDigestValue,
+        'content-length' => strlen($requestBody),
+        'content-type' => 'application/json',
+      ]);
+    }
 
-  $serializer = new CompactSerializer();
-  $token = $serializer->serialize($jws);
+    if(isset($headers['Authorization'])) {
+      $messageSignatureBase['authorization'] = $headers['Authorization'];
+    }
 
-  echo $token."\n\n";
+    # https://www.ietf.org/archive/id/draft-ietf-httpbis-message-signatures-09.html#name-rsassa-pss-using-sha-512
+    // fromMembers is called fromList in later versions
+    $signatureParams = StructuredFields\InnerList::fromMembers(array_keys($messageSignatureBase), [
+      'created' => $createdAt,
+      'keyid' => $this->privateKeyFile,
+      'alg' => ($this->method == 'pss' ? 'rsa-pss-sha512' : 'rsa-v1_5-sha256'),
+    ]);
+    
+    $messageSignatureString = '';
+    foreach($messageSignatureBase as $k=>$v) {
+      $messageSignatureString .= '"'.$k.'": '.$v."\n";
+    }
+    
+    $messageSignatureString .= '"@signature-params": '.$signatureParams->toHttpValue();
 
-  $headers = [
-    'Content-Type: application/json',
-    'Detached-JWS: ' . $token,
-  ];
+    #echo $messageSignatureString."\n\n";
+    
+    #$messageSignatureString = 'Hello';
 
-  if($accessToken) {
-    $headers[] = 'Authorization: GNAP ' . $accessToken;
+    $hashBytes = hash($this->method == 'pss' ? 'sha512' : 'sha256', $messageSignatureString, true);
+    $signature = $this->rsa->sign($hashBytes);
+    $signatureByteSequence = StructuredFields\ByteSequence::fromDecoded($signature);
+
+    $signatureInput = StructuredFields\Dictionary::fromPairs([['sig', $signatureByteSequence]]);
+    
+    // https://www.ietf.org/archive/id/draft-ietf-httpbis-message-signatures-09.html#name-the-signature-http-field
+    $byteSequence = StructuredFields\ByteSequence::fromDecoded($signature);
+    $dictionary = StructuredFields\Dictionary::fromPairs([['sig', $byteSequence]]);
+    
+    $signatureParamsDictionary = StructuredFields\Dictionary::fromPairs([['sig', $signatureParams]]);
+
+    #echo $requestBody."\n";
+    #echo 'Signature: '.$dictionary->toHttpValue();
+    #echo "\n";
+
+    $headers['Signature'] = $dictionary->toHttpValue();
+    $headers['Signature-Input'] = $signatureParamsDictionary->toHttpValue();
+    $headers['Accept'] = 'application/json';
+    $headers['Content-Type'] = 'application/json';
+    
+    if($params) {
+      $headers['Content-Length'] = strlen($requestBody);
+      $headers['Content-Digest'] = $bodyDigestValue;
+    }
+
+    $headerStr = [];
+    foreach($headers as $k=>$v) {
+      $headerStr[] = "$k: $v";
+    }
+    $headers = $headerStr;
+    
+//     echo "POST ".parse_url($uri, PHP_URL_PATH)." HTTP/1.1\r\n";
+//     echo "Host: ".parse_url($uri, PHP_URL_HOST)."\r\n";
+//     echo implode("\n", $headers);
+//     if($params)
+//       echo "\r\n\r\n$requestBody\n\n";
+// 
+//     echo "Public JWK:\n";
+//     echo json_encode($this->jwk->toPublic()->all(), JSON_UNESCAPED_SLASHES);
+//     echo "\n\n";
+
+    $ch = curl_init($uri);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    if($params)
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $response = curl_exec($ch);
+    
+    // if(($code=curl_getinfo($ch, CURLINFO_RESPONSE_CODE)) != 200) {
+    //   echo "\n\nERROR: ".$code."\n";
+    //   echo $response."\n";
+    //   die();
+    // }
+    // 
+    echo "Response\n";
+    echo $response."\n\n";
+
+    return json_decode($response, true);
   }
-
-  echo "Making request: $url\n";
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  $response = curl_exec($ch);
-  echo "Response code " . curl_getinfo($ch, CURLINFO_HTTP_CODE) . "\n";
-  return json_decode($response, true);
-}
-
-function base64_url_encode($binary_data) {
-  return strtr(rtrim(base64_encode($binary_data), '='), '+/', '-_');
+  
+  private function _clientProperties() {
+    return [
+      'key' => [
+        'proof' => 'httpsig',
+        'jwk' => $this->jwk->toPublic()->all(),
+      ],
+      'display' => [
+        'name' => $this->name,
+      ],
+    ];
+  }
 }
